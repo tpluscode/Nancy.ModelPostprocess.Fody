f7e2c927ebc13c130e2f3c6f7a75dac674520324
tpluscode <tomasz@t-code.pl>
2014-01-29 15:22:03 +0100

    add Nancy

---
+++ AssemblyToProcess/SampleModule.cs
@@ -0,0 +1,13 @@
+﻿using Nancy;
+
+namespace AssemblyToProcess
+{
+    public class SampleModule : NancyModule
+    {
+        public SampleModule()
+        {
+            Get["Model"] = p => new SampleModel { SomeValue = "Set in module" };
+            Get["AString"] = p => "some arbitrary value";
+        }
+    }
+}
