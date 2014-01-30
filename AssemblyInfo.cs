a01a3551606a88ae82cb74617e3bff56fa0538bb
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2014-01-30 15:27:27 +0100

    extension method

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -2,5 +2,5 @@
 
 [assembly: AssemblyTitle("Nancy.ModelPostprocess")]
 [assembly: AssemblyProduct("Nancy.ModelPostprocess")]
-[assembly: AssemblyVersion("0.1.3.0")]
-[assembly: AssemblyFileVersion("0.1.3.0")]
+[assembly: AssemblyVersion("0.1.4")]
+[assembly: AssemblyFileVersion("0.1.4")]
