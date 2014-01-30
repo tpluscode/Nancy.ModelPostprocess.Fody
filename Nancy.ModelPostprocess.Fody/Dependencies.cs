697c19adb1891ebddcd2852912cda100d312fbd2
tpluscode <tomasz@t-code.pl>
2014-01-30 13:08:21 +0100

    implmented weaver

---
+++ Nancy.ModelPostprocess.Fody/Dependencies.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using Mono.Cecil;
+
+namespace Nancy.ModelPostprocess.Fody
+{
+    public partial class ModuleWeaver
+    {
+        private AssemblyDefinition NancyAssembly;
+        private AssemblyDefinition ReferencedAssembly;
+
+        private void LoadDependencies()
+        {
+            NancyAssembly = LoadAssemblyReference("Nancy");
+            ReferencedAssembly = LoadAssemblyReference("Nancy.ModelPostprocess");
+        }
+
+        private AssemblyDefinition LoadAssemblyReference(string assemblyFullName)
+        {
+            var existingReference = ModuleDefinition.AssemblyReferences.FirstOrDefault(x => x.Name == assemblyFullName);
+
+            if (existingReference != null)
+            {
+                return AssemblyResolver.Resolve(existingReference);
+            }
+
+            var reference = AssemblyResolver.Resolve(assemblyFullName);
+            if (reference != null)
+            {
+                return reference;
+            }
+
+            throw new Exception(string.Format("Could not resolve a reference to {0}.", assemblyFullName));
+        }
+    }
+}
---
+++ Nancy.ModelPostprocess.Fody/ImportedTypes.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Mono.Cecil;
+
+namespace Nancy.ModelPostprocess.Fody
+{
+    public partial class ModuleWeaver
+    {
+        private TypeReference InjectorType;
+        private TypeReference RouteBuilderType;
+        private MethodReference InjectMethod;
+        private MethodReference AsyncInjectMethod;
+
+        private void LoadTypes()
+        {
+            var nancyModuleType = NancyAssembly.MainModule.Types.Single(t => t.Name == "NancyModule");
+            var routeBuilderTypeDefinition = nancyModuleType.NestedTypes.Single(nt => nt.Name == "RouteBuilder");
+
+            var injectorType = ReferencedAssembly.MainModule.Types.Single(type => type.Name == "ModelPostprocessor");
+            InjectorType = ModuleDefinition.Import(injectorType);
+            RouteBuilderType = ModuleDefinition.Import(routeBuilderTypeDefinition);
+
+            InjectMethod = ModuleDefinition.Import(injectorType.FindMethod("WrapRoute", "Func`2", InjectorType.Name, nancyModuleType.Name));
+            AsyncInjectMethod = ModuleDefinition.Import(injectorType.FindMethod("WrapAsyncRoute", "Func`3", InjectorType.Name, nancyModuleType.Name));
+        }
+    }
+}
