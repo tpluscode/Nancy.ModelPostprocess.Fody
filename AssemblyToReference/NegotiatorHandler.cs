d5e86e48dcee8b31c650744a4783f4b01d3d90fa
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2014-01-30 15:59:16 +0100

    implement handling of negotiated responses

---
+++ AssemblyToReference/NegotiatorHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using Nancy.Responses.Negotiation;
+
+namespace Nancy.ModelPostprocess
+{
+    internal class NegotiatorHandler : IModelHandler<Negotiator>
+    {
+        private readonly IModelPostprocessor _postprocessor;
+
+        public NegotiatorHandler(IModelPostprocessor postprocessor)
+        {
+            _postprocessor = postprocessor;
+        }
+
+        public void Postprocess(Negotiator model, NancyModule module)
+        {
+            var negotiationContext = model.NegotiationContext;
+            _postprocessor.Postprocess(negotiationContext.DefaultModel, module);
+            ProcessMediaRangeMappings(negotiationContext, module);
+        }
+
+        private void ProcessMediaRangeMappings(NegotiationContext negotiationContext, NancyModule module)
+        {
+            var mappingProcessed = from mapping in negotiationContext.MediaRangeModelMappings
+                                   select new
+                                           {
+                                               mapping.Key,
+                                               Value = new Func<dynamic>(() => _postprocessor.Postprocess(mapping.Value(), module))
+                                           };
+
+            negotiationContext.MediaRangeModelMappings = mappingProcessed.ToDictionary(p => p.Key, p => p.Value);
+        }
+    }
+}
